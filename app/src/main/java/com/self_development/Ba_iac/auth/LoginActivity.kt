2933a695d59d82198a39b630338cd6bf778b4bef
LimSuBin <tntls00@naver.com>
2022-03-02 00:16:37 +0900

    fuck

--- app/src/main/java/com/self_development/Ba_iac/auth/LoginActivity.kt
+++ app/src/main/java/com/self_development/Ba_iac/auth/LoginActivity.kt
@@ -3,14 +3,18 @@ package com.self_development.Ba_iac.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.self_development.Ba_iac.MainActivity
 import com.self_development.Ba_iac.R
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
 
@@ -22,6 +26,36 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         auth = Firebase.auth
+
+        val findPassword = findViewById<TextView>(R.id.findPassword)
+        findPassword.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.password_setting, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("passwordChange")
+
+            val  mAlertDialog = mBuilder.show()
+
+            val okBtn = mDialogView.findViewById<Button>(R.id.okBtn)
+            val noBtn = mDialogView.findViewById<Button>(R.id.noBtn)
+            val putEmail = mDialogView.findViewById<EditText>(R.id.putEmailArea)
+
+
+            okBtn.setOnClickListener {
+                FirebaseAuth.getInstance().sendPasswordResetEmail(putEmail.text.toString()).addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "비밀번호 변경 메일을 전송했습니다", Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(this, task.exception.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+                mAlertDialog.dismiss()
+            }
+            noBtn.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+
+        }
 
         val loginBtn = findViewById<Button>(R.id.loginBtn)
         loginBtn.setOnClickListener {
@@ -48,4 +82,8 @@ class LoginActivity : AppCompatActivity() {
 
 
     }
+
+
+
+
 }
