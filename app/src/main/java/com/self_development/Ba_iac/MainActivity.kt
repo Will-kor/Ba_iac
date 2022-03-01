9e815ba47732525e3876d85d9cc4252d66ca5282
Will-kor <0koreawill0@gmail.com>
2022-03-02 01:29:26 +0900

    Feat: banner 기능 추가
    MainActivity.kt에 firebase import 및 slide_in_right.xml, slide_out_left.xml animation 파일 추가함.
     build.gradle의 plugin -
     id 'kotlin-android-extensions' ,
     dependencies -
     implementation 'com.github.bumptech.glide:glide:4.11.0' 추가함.
     자세한 코드 및 xml은 MainActivity.kt 참고.
    
    작업자 Will-kor

--- app/src/main/java/com/self_development/Ba_iac/MainActivity.kt
+++ app/src/main/java/com/self_development/Ba_iac/MainActivity.kt
@@ -3,21 +3,30 @@ package com.self_development.Ba_iac
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import android.widget.*
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.self_development.Ba_iac.auth.AuthActivity
 import com.self_development.Ba_iac.setting.MyPageActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var storage = FirebaseStorage.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val viewFlipper = findViewById<ViewFlipper>(R.id.banner)
+        val storageRef = storage.reference
         val logout = findViewById<Button>(R.id.logoutBtn)
+
         logout.setOnClickListener {
 
             val auth = Firebase.auth
@@ -36,5 +45,20 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        for(i in 0..4) {
+            storageRef.child("banner/" + i + ".jpg").downloadUrl.addOnSuccessListener {
+                val imageView = ImageView(this)
+                Glide.with(this@MainActivity).load(it).into(imageView)
+                viewFlipper.addView(imageView)
+                viewFlipper.flipInterval = 8000
+                viewFlipper.isAutoStart = true
+                viewFlipper.startFlipping()
+                viewFlipper.setInAnimation(this, R.anim.slide_in_right)
+                viewFlipper.setOutAnimation(this, R.anim.slide_out_left)
+            }.addOnFailureListener {
+
+            }
+        }
     }
 }
+
