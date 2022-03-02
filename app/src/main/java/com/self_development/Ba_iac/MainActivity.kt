1825d9891004249e8100e2f13b4e5ff8442f6cbf
Will-kor <0koreawill0@gmail.com>
2022-03-02 21:12:35 +0900

    Feat: banner 시각화 기능 및 터치 추가함
    slide_in_left.xml, slide_out_right.xml animation 파일 추가함.
    activity_main.xml 시각화 기능을 위한 arrow text 추가함.
    MainActivity.kt banner애 해당하는 코드 추가함.
    
    작업자 Will-kor

--- app/src/main/java/com/self_development/Ba_iac/MainActivity.kt
+++ app/src/main/java/com/self_development/Ba_iac/MainActivity.kt
@@ -1,31 +1,41 @@
 package com.self_development.Ba_iac
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.*
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.self_development.Ba_iac.auth.AuthActivity
 import com.self_development.Ba_iac.setting.MyPageActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private var storage = FirebaseStorage.getInstance()
+    var currentBIndex = 0
+    val countBIndexes = 4 //배너 이미지 개수
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewFlipper = findViewById<ViewFlipper>(R.id.banner)
         val storageRef = storage.reference
         val logout = findViewById<Button>(R.id.logoutBtn)
+        val viewFlipper = findViewById<ViewFlipper>(R.id.banner)
+        val bannerLarrow = findViewById<TextView>(R.id.leftarrow)
+        val bannerRarrow = findViewById<TextView>(R.id.rightarrow)
+
+        var downX = 0f
+        var upX = 0f
 
         logout.setOnClickListener {
 
@@ -45,12 +55,12 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        for(i in 0..4) {
+        for(i in 0..countBIndexes) {
             storageRef.child("banner/" + i + ".jpg").downloadUrl.addOnSuccessListener {
                 val imageView = ImageView(this)
-                Glide.with(this@MainActivity).load(it).into(imageView)
+                Glide.with(this).load(it).into(imageView)
                 viewFlipper.addView(imageView)
-                viewFlipper.flipInterval = 8000
+                viewFlipper.setFlipInterval(8000)
                 viewFlipper.isAutoStart = true
                 viewFlipper.startFlipping()
                 viewFlipper.setInAnimation(this, R.anim.slide_in_right)
@@ -59,6 +69,67 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+
+        viewFlipper.setOnTouchListener { v: View, event:MotionEvent ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    downX = event.x
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    upX = event.x
+                    if (upX < downX) {
+                        viewFlipper.setInAnimation(this, R.anim.slide_in_right)
+                        viewFlipper.setOutAnimation(this, R.anim.slide_out_left)
+                        if (currentBIndex < countBIndexes - 1) {
+                            viewFlipper.showNext()
+                            currentBIndex++
+                                if (currentBIndex == countBIndexes - 1) {
+                                    bannerRarrow.setTextColor(Color.parseColor("#d3d3d3"))
+                                }
+                                else if (currentBIndex == 0) {
+                                    bannerLarrow.setTextColor(Color.parseColor("#d3d3d3"))
+                                }
+                                else{
+                                    bannerRarrow.setTextColor(Color.parseColor("#202020"))
+                                    bannerLarrow.setTextColor(Color.parseColor("#202020"))
+                                }
+                        }
+                    }
+                    else if (upX > downX) {
+                        viewFlipper.setInAnimation(this, R.anim.slide_in_left)
+                        viewFlipper.setOutAnimation(this, R.anim.slide_out_right)
+                        if (currentBIndex > 0) {
+                            viewFlipper.showPrevious()
+                            currentBIndex--
+                                if (currentBIndex == 2) {
+                                    bannerRarrow.setTextColor(Color.parseColor("#d3d3d3"))
+                                }
+                                else if (currentBIndex == 0) {
+                                    bannerLarrow.setTextColor(Color.parseColor("#d3d3d3"))
+                                }
+                                else{
+                                    bannerRarrow.setTextColor(Color.parseColor("#202020"))
+                                    bannerLarrow.setTextColor(Color.parseColor("#202020"))
+                                }
+                        }
+                    }
+                    viewFlipper.setInAnimation(this, R.anim.slide_in_right)
+                    viewFlipper.setOutAnimation(this, R.anim.slide_out_left)
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
+        bannerLarrow.setText("<")
+        bannerLarrow.setTextSize(18F)
+        bannerLarrow.setTextColor(Color.parseColor("#d3d3d3"))
+
+        bannerRarrow.setText(">")
+        bannerRarrow.setTextSize(18F)
+        bannerRarrow.setTextColor(Color.parseColor("#202020"))
     }
 }
-
--- app/src/main/java/com/self_development/Ba_iac/auth/JoinActivity.kt
+++ app/src/main/java/com/self_development/Ba_iac/auth/JoinActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -24,69 +22,37 @@ class JoinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
-        val email = findViewById<EditText>(R.id.emailArea)
-        val pwd = findViewById<EditText>(R.id.passwordArea)
-        val pwdPwd = findViewById<EditText>(R.id.passwordPwdArea)
-
         auth = Firebase.auth
 
         val nextBtn = findViewById<Button>(R.id.nextBtn)
         nextBtn.setOnClickListener {
 
-            val emailCheck = email.text.toString()
-            val pwdCheck = pwd.text.toString()
-            val pwdCheckCheck = pwdPwd.text.toString()
+            val email = findViewById<EditText>(R.id.emailArea)
+            val pwd = findViewById<EditText>(R.id.passwordArea)
 
-            if (emailCheck.isEmpty() || pwdCheck.isEmpty() || pwdCheckCheck.isEmpty()) {
+            //Log.d(TAG, email.text.toString())
+            //Log.d(TAG, pwd.text.toString())
 
-                Toast.makeText(this, "모든 정보를 입력하여 주십시오", Toast.LENGTH_SHORT).show()
+            auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        Log.d(TAG, user?.uid.toString())
 
-            } else {
+                        val intent = Intent(this, NameActivity::class.java)
+                        startActivity(intent)
 
-                createUser()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
 
-            }
-
-            if (pwdCheck.equals(pwdCheckCheck)) {
-
-                createUser()
-
-            } else {
-
-                Toast.makeText(this, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
-
-            }
-
+                    }
+                }
 
         }
 
 
     }
-
-    private fun createUser() {
-
-        val email = findViewById<EditText>(R.id.emailArea)
-        val pwd = findViewById<EditText>(R.id.passwordArea)
-
-        auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    Log.d(TAG, user?.uid.toString())
-
-                    val intent = Intent(this, NameActivity::class.java)
-                    startActivity(intent)
-
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(this, "이메일 중복", Toast.LENGTH_SHORT).show()
-
-                }
-            }
-
-    }
-
 }
