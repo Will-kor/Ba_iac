38f9278312df8b48b739a251baaa8100d823a9d8
LimSuBin <tntls00@naver.com>
2022-03-05 22:45:58 +0900

    1. mainFun 내 all Fragment   - viewBinding 작업   - 화면전환 작업(setOnClickListener)   - LinearLayout으로 모자 씌워주기   - mainFunActuvity에 상단바 틀 임시 적용

--- app/src/main/java/com/self_development/ba_iac/mainfuntion/communityFragment.kt
+++ app/src/main/java/com/self_development/ba_iac/mainfuntion/communityFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.findNavController
+import com.self_development.ba_iac.R
 import com.self_development.ba_iac.databinding.FragmentCommunityBinding
 
 class communityFragment : Fragment() {
@@ -25,8 +27,21 @@ class communityFragment : Fragment() {
     ): View? {
 
         _binding = FragmentCommunityBinding.inflate(inflater, container, false)
-        binding.communityTap.setOnClickListener {
-            Toast.makeText(context, "ㅎㅇ", Toast.LENGTH_SHORT).show()
+
+        binding.profileTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_communityFragment_to_studentProfileFragment)
+        }
+
+        binding.organizeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_communityFragment_to_organizeFragment)
+        }
+
+        binding.raidBossTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_communityFragment_to_raidBossFragment)
+        }
+
+        binding.questTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_communityFragment_to_questFragment)
         }
 
         return binding.root
--- app/src/main/java/com/self_development/ba_iac/mainfuntion/raidBossFragment.kt
+++ app/src/main/java/com/self_development/ba_iac/mainfuntion/raidBossFragment.kt
@@ -5,56 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.self_development.ba_iac.R
+import com.self_development.ba_iac.databinding.FragmentCommunityBinding
+import com.self_development.ba_iac.databinding.FragmentRaidBossBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [raidBossFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class raidBossFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentRaidBossBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_raid_boss, container, false)
+
+        _binding = FragmentRaidBossBinding.inflate(inflater, container, false)
+
+        binding.profileTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_raidBossFragment_to_studentProfileFragment)
+        }
+
+        binding.organizeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_raidBossFragment_to_organizeFragment)
+        }
+
+        binding.communityTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_raidBossFragment_to_communityFragment)
+        }
+
+        binding.questTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_raidBossFragment_to_questFragment)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment raidBossFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            raidBossFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
 }
