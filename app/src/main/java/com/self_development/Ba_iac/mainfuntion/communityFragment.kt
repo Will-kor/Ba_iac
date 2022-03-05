79176125527c981bc6fb197721988620c8113995
LimSuBin <tntls00@naver.com>
2022-03-05 18:16:14 +0900

    1. communityFragment에 viewbinding 및 버튼 클릭이벤트 추가   - 에러 존나 남.

--- app/src/main/java/com/self_development/Ba_iac/mainfuntion/communityFragment.kt
+++ app/src/main/java/com/self_development/Ba_iac/mainfuntion/communityFragment.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import com.self_development.Ba_iac.R
 import com.self_development.Ba_iac.databinding.FragmentCommunityBinding
 
 class communityFragment : Fragment() {
+
+    private var _binding: FragmentCommunityBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +26,18 @@ class communityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_community, container, false)
+        _binding = FragmentCommunityBinding.inflate(inflater, container, false)
+        binding.communityTap.setOnClickListener {
+            Toast.makeText(context, "ㅎㅇ", Toast.LENGTH_SHORT).show()
+        }
+
+        return binding.root
 
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
